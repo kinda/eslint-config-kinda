6afb4b50b049077cf2261de6973e6d2e626a31bd
Manuel Vila <mvila@3base.com>
2015-08-11 12:41:48 +0800

    remove 'object-shorthand' rule

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ module.exports = {
     'no-unused-expressions': [2],
     'no-unused-vars': [2],
     'no-var': [2],
-    'object-shorthand': [2],
     'operator-linebreak': [2],
     'quotes': [2, 'single'],
     'radix': [2],
